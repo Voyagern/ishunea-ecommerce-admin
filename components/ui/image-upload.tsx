8a739681b4217e92651af44813c6241b90c3c640
Voyager_n <ol.turevici@gmail.com>
2024-09-12 13:52:07 +0300

    ITWORKS URA billboard template

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,80 +1,88 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import { CldUploadWidget } from "next-cloudinary";
-import { ImagePlus, Trash } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
 
+import { CldUploadWidget } from "next-cloudinary"
 
-interface imageUploadProps {
+interface ImageUploadProps {
     disabled?: boolean;
     onChange: (value: string) => void;
     onRemove: (value: string) => void;
-    value: string[];
+    value: string[]
 }
-
-const ImageUpload: React.FC<imageUploadProps> = ({
+const ImageUpload: React.FC<ImageUploadProps> = ({
                                                      disabled,
                                                      onChange,
                                                      onRemove,
-                                                     value,
+                                                     value
                                                  }) => {
-    const onUpload = (result: any) => {
-        console.log(result); // Выводим результат в консоль
-        if (result && result.info && result.info.secure_url) {
-            onChange(result.info.secure_url);
-        } else {
-            console.error("Ошибка получения URL изображения", result);
-        }
-    };
+    const [isMounted, setIsMounted] = useState(false);
 
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url);
+    }
+
+    if(!isMounted)
+    {
+        return null;
+    }
 
     return (
-
-            <div>
-                <div className="flex items-center gap-4 mb-4">
-                    {value.map((url) => (
-                        <div
-                            key={url}
-                            className="relative w-[200px] h-[200px] overflow-hidden"
-                        >
-                            <div className="absolute z-10 top-2 right-2">
-                                <Button
-                                    type="button"
-                                    onClick={() => onRemove(url)}
-                                    variant="destructive"
-                                    size="icon"
-                                >
-                                    <Trash className="w-4 h-4" />
-                                </Button>
-                            </div>
-                            <Image fill className="object-cover" alt="Image" src={url} />
-                        </div>
-                    ))}
-                </div>
-                <CldUploadWidget onUpload={onUpload} uploadPreset="rxmoscur">
-                    {({ open }) => {
-                        const onClick = () => {
-                            open();
-                        };
-                        return (
+        <div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map((url) => (
+                    <div key={url}
+                         className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+                    >
+                        <div className="z-10 absolute top-2 right-2">
                             <Button
                                 type="button"
-                                disabled={disabled}
-                                variant="secondary"
-                                onClick={onClick}
+                                onClick={() => onRemove(url)}
+                                variant="destructive"
+                                size="icon"
                             >
-                                <ImagePlus className="w-4 h-4 mr-2" />
-                                Upload an Image
+                                <Trash className="h-4 w-4"/>
                             </Button>
-                        );
-                    }}
-                </CldUploadWidget>
-            </div>
+                        </div>
+                        <Image
+                            fill
+                            className="object-cover"
+                            alt="Image"
+                            src={url}
+                        />
 
-    );
-};
+                    </div>
+                ))}
+            </div>
+            <CldUploadWidget
+                onSuccess={onUpload} uploadPreset="rxmoscur"
+            >
+                {({ open }) => {
+                    const onClick = () => {
+                        open();
+                    }
+                    return (
+                        <Button
+                            type="button"
+                            disabled={disabled}
+                            variant="secondary"
+                            onClick={onClick}
+                        >
+                            <ImagePlus className="h-4 w-4 mr-2"/>
+                            Upload an Image
+                        </Button>
+                    )
+                }}
+            </CldUploadWidget>
+        </div>
+    )
+}
 
 export default ImageUpload;
