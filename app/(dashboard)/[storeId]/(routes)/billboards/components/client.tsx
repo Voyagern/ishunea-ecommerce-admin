dde14eed9092eeeaa47a14028c5fd4b246d1b771
Voyager_n <ol.turevici@gmail.com>
2024-09-12 19:27:19 +0300

    data table end

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import {useParams, useRouter} from "next/navigation";
 import React from "react";
 import {BillboardColumn, columns} from "@/app/(dashboard)/[storeId]/(routes)/billboards/components/columns";
 import {DataTable} from "@/components/ui/data-table";
+import {ApiList} from "@/components/ui/api-list";
 
 interface BillboardClientProps{
     data:BillboardColumn[]
@@ -34,6 +35,9 @@ export const BillboardClient:React.FC<BillboardClientProps>=({
         </div>
             <Separator/>
             <DataTable searchKey="label" columns={columns} data={data} />
+            <Heading title="API" description="API calls for billboards"/>
+            <Separator/>
+            <ApiList entityName="billboards" entityIdName="billboardId"/>
         </>
     )
 }
